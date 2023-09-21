1f76d5137390ecfd562ccefb0e3df6a9f52f8ac7
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-09-21 22:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+// Find the element with the id "level"
+const element = document.getElementById("level");
+
+// Initialize a variable to count the DOM level
+let domLevel = 0;
+
+// Traverse the DOM tree upwards from the target element
+let currentElement = element;
+while (currentElement && currentElement !== document.documentElement) {
+  currentElement = currentElement.parentElement;
+  domLevel++;
+}
+
+// Display the DOM level using the alert function
+alert(`The level of the element is: ${domLevel}`);
